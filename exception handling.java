4bf5f584d16aec024e132ab47c4956c2fa468b38
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-11 14:06:50 +0530

    Create exception handling.java

---
+++ exception handling.java
@@ -0,0 +1,16 @@
+public class TryCatchExample2 {  
+  
+    public static void main(String[] args) {  
+        try  
+        {  
+        int data=50/0; //may throw exception   
+        }  
+            //handling the exception  
+        catch(ArithmeticException e)  
+        {  
+            System.out.println(e);  
+        }  
+        System.out.println("rest of the code");  
+    }  
+      
+}  
