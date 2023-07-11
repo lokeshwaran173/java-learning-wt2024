576d2014afbd401297873d6ed7f07737f73c7b6e
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-11 09:10:36 +0530

    Add files via upload

---
+++ area of rectangle in java method 1.java
@@ -0,0 +1,14 @@
+public class sum{
+    public static void main(String args[]){
+        prod();
+    }
+    public static void prod()
+    //1 st  method of prodect
+    {
+        int a = 3; int b =4;
+        int c = a*b;
+        System.out.println(c);
+    }
+
+}
+
---
+++ print name or value.java
@@ -0,0 +1,7 @@
+public class Nav{
+    public static void main(String args[]){
+        System.out.println("rit");
+    }
+
+}
+
