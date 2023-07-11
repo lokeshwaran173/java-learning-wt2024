c35cf2156677850d0456834a992208a039f579ee
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-11 14:07:46 +0530

    Create static code.java

---
+++ static code.java
@@ -0,0 +1,12 @@
+public class Rit {
+    public static void main(String args[]) {
+        fn();
+        fn();
+        fn();
+    }
+ static int i = 1;
+    public static void fn() {
+       
+        System.out.println(i++);
+    }
+}
