e5e91d82164c6b8e68e09499d0c3a85ff53ee2ed
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-13 09:23:06 +0530

    Create if else.java

---
+++ if else.java
@@ -0,0 +1,11 @@
+public class Student {  
+public static void main(String[] args) {  
+int x = 10;  
+int y = 12;  
+if(x+y < 10) {  
+System.out.println("x + y is less than      10");  
+}   else {  
+System.out.println("x + y is greater than 20");  
+}  
+}  
+}  
