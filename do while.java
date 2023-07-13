7450e54a71a95cb5496485066e20618576616ca6
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-13 10:10:04 +0530

    Create do while.java

---
+++ do while.java
@@ -0,0 +1,10 @@
+public class Calculation {    
+public static void main(String[] args) {        
+int i = 0;    
+System.out.println("Printing the list of first 10 even numbers \n");    
+do {    
+System.out.println(i);    
+i = i + 2;    
+}while(i<=10);    
+}    
+}    
