4b26f0d83f8e482162e114d481a912440d9077f5
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-13 13:49:20 +0530

    Create continue jump.java

---
+++ continue jump.java
@@ -0,0 +1,18 @@
+public class ContinueExample {  
+  
+public static void main(String[] args) {  
+ 
+  
+for(int i = 0; i<= 2; i++) {  
+  
+for (int j = i; j<=5; j++) {  
+  
+if(j == 4) {  
+continue;  
+}  
+System.out.println(j);  
+}  
+}  
+}  
+  
+}  
