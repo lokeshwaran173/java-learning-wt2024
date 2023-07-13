106959a3d3bc8964eb87994cf10f0e765a6761ae
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-13 10:00:38 +0530

    Create for each loop.java

---
+++ for each loop.java
@@ -0,0 +1,9 @@
+public class Calculation {    
+public static void main(String[] args) {    
+String[] names = {"Java","C","C++","Python","JavaScript"};    
+System.out.println("Printing the content of the array names:\n");    
+for(String name:names) {    
+System.out.println(name);    
+}    
+}    
+}    
