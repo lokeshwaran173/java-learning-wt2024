5fa1f8bea33535f7bdeeb2aac22edd13e07f00c5
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-13 11:50:24 +0530

    Create even odd.java

---
+++ even odd.java
@@ -0,0 +1,18 @@
+public class NumberCount {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        System.out.println("Number of even numbers: " + evenCount);
+        System.out.println("Number of odd numbers: " + oddCount);
+    }
+
+  
+}
