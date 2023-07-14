17486532234574081f6ef7a3fc6195fa4d2f7bed
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-14 11:44:46 +0530

    Create if else grading.java

---
+++ if else grading.java
@@ -0,0 +1,12 @@
+public class grade {
+    public static void main(String args[]) {
+        int marks = 70;
+        if (marks<50){
+            System.out.println("fail");
+        }else {
+            System.out.println("good grade");
+        }
+    }
+    
+}
+        
