71fb1d742a885212101ff3e45f17fd546a5324ad
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-14 10:57:23 +0530

    Create leap year.java

---
+++ leap year.java
@@ -0,0 +1,11 @@
+public class LeapYearExample {    
+public static void main(String[] args) {    
+    int year=2020;    
+    if(((year % 4 ==0) && (year % 100 !=0)) || (year % 400==0)){  
+        System.out.println("LEAP YEAR");  
+    }  
+    else{  
+        System.out.println("COMMON YEAR");  
+    }  
+}    
+}    
