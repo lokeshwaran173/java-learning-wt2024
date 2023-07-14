fd3444721d2ba9cce7d683765bbf8427942063ad
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-14 11:29:14 +0530

    Create ternary operator.java

---
+++ ternary operator.java
@@ -0,0 +1,8 @@
+public class IfElseTernaryExample {    
+public static void main(String[] args) {    
+    int number=13;    
+    //Using ternary operator  
+    String output=(number%2==0)?"even number":"odd number";    
+    System.out.println(output);  
+}    
+}    
