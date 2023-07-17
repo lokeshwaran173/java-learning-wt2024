863410a5423658138d2312c50b810cb41833469b
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-17 14:30:44 +0530

    Create pract.java

---
+++ pract.java
@@ -0,0 +1,12 @@
+public class PositiveNegativeExample {    
+public static void main(String[] args) {    
+    int number=-13;    
+    if(number>0){  
+    System.out.println("POSITIVE");  
+    }else if(number<0){  
+    System.out.println("NEGATIVE");  
+    }else{  
+    System.out.println("ZERO");  
+   }  
+}    
+} 
