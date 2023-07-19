60713a15673306c4e2836224a43b5fa96ba25b42
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-07-19 08:30:15 +0530

    Create multiline comment.java

---
+++ multiline comment.java
@@ -0,0 +1,11 @@
+public class CommentExample2 {    
+public static void main(String[] args) {    
+/* Let's declare and  
+ print variable in java. */    
+  int i=10;    
+    System.out.println(i);    
+/* float j = 5.9; 
+    float k = 4.4; 
+    System.out.println( j + k ); */    
+}    
+}    
