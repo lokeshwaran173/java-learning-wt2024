78ba8d597397aee0091e3e8ecc40584c9bbdea9f
LOKESHWARAN.S <94963133+lokeshwaran173@users.noreply.github.com>
2023-11-14 18:16:45 +0530

    Create nestle.java

---
+++ nestle.java
@@ -0,0 +1,12 @@
+public class JavaNestedIfExample {    
+public static void main(String[] args) {    
+    //Creating two variables for age and weight  
+    int age=20;  
+    int weight=80;    
+    //applying condition on age and weight  
+    if(age>=18){    
+        if(weight>50){  
+            System.out.println("You are eligible to donate blood");  
+        }    
+    }    
+}}  
